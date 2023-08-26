d2815f98b54c9f81890ea0b8585d948700260892
naufalbakh <naufalbakhtiar20@gmail.com>
2023-08-26 13:29:22 +0700

    update interface

--- js/scripts.js
+++ js/scripts.js
@@ -1,37 +1,39 @@
 /*!
-* Start Bootstrap - Shop Homepage v5.0.5 (https://startbootstrap.com/template/shop-homepage)
-* Copyright 2013-2022 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-shop-homepage/blob/master/LICENSE)
-*/
+ * Start Bootstrap - Shop Homepage v5.0.5 (https://startbootstrap.com/template/shop-homepage)
+ * Copyright 2013-2022 Start Bootstrap
+ * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-shop-homepage/blob/master/LICENSE)
+ */
 // This file is intentionally blank
 // Use this file to add JavaScript to your project
 
+// Fungsi untuk mengaktifkan fitur pencarian
+function searchSubjects() {
+  const searchInput = document.getElementById("searchInput");
+  const subjects = document.querySelectorAll("section");
 
+  searchInput.addEventListener("input", function () {
+    const searchText = searchInput.value.toLowerCase();
 
-function openSUBJ(evt, subjName) {
-    var i, tabcontent, tablinks;
-    tabcontent = document.getElementsByClassName("tabcontent");
-    for (i = 0; i < tabcontent.length; i++) {
-        tabcontent[i].style.display = "none";
-    }
-    tablinks = document.getElementsByClassName("tablinks");
-    for (i = 0; i < tablinks.length; i++) {
-        tablinks[i].className = tablinks[i].className.replace(" active", "");
-
-    }
-    document.getElementById(subjName).style.display = "block";
-    evt.currentTarget.className += " active";
-
+    subjects.forEach((subject) => {
+      const subjectName = subject.querySelector("h3").textContent.toLowerCase();
+      if (subjectName.includes(searchText)) {
+        subject.style.display = "block";
+      } else {
+        subject.style.display = "none";
+      }
+    });
+  });
 }
-// Get the element with id="defaultOpen" and click on it
-document.getElementById("defaultOpen").click();
+
+// Panggil fungsi pencarian saat halaman dimuat
+window.onload = searchSubjects;
 
 function openNav() {
-    document.getElementById("mySidenav").style.width = "300px";
+  document.getElementById("mySidenav").style.width = "300px";
 }
 
 function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
+  document.getElementById("mySidenav").style.width = "0";
 }
 
 // function searchBar() {
